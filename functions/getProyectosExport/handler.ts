bcb72d297db5091cafab3263b2081dddb8939c25
cuevacelis <cuevacelis@hotmail.com>
2024-10-11 17:42:51 -0500

    feat: finish handler export proyectos

--- functions/getProyectosExport/handler.ts
+++ functions/getProyectosExport/handler.ts
@@ -1,12 +1,13 @@
-// handler.ts
-import { APIGatewayProxyEvent } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { sql } from "kysely";
 import getDbPostgres from "../../db/db-postgres";
 import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 
 const lambdaClient = new LambdaClient({ region: "us-east-1" });
 
-export const handler = async (event: APIGatewayProxyEvent) => {
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   try {
     const { userId, prefixNameFile, email } = JSON.parse(event.body || "{}");
 
@@ -27,42 +28,52 @@ export const handler = async (event: APIGatewayProxyEvent) => {
           ${100},
           ${1},
           ${""}
-        )
-      `.as("result")
+        )`.as("result")
       )
       .selectAll()
       .execute();
 
+    // Verificar si resultSQL tiene datos
+    if (!resultSQL || resultSQL.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          error: "No se encontraron presupuestos para exportar.",
+        }),
+      };
+    }
+
     try {
+      const payload = {
+        Records: [
+          {
+            body: JSON.stringify({
+              type: "excel-export",
+              dataSend: {
+                data: resultSQL.map((object: any) => ({
+                  Código: object.pre_codigo || "",
+                  Usuario: object.usu_nomapellidos,
+                  Nombre: object.pre_nombre,
+                  "Razón social": object.cli_nomaperazsocial,
+                  Jornal: object.pre_jornal,
+                  Fecha: object.pre_fechorregistro,
+                })),
+                userId: userId,
+                prefixNameFile: prefixNameFile,
+                email: email,
+              },
+            }),
+          },
+        ],
+      };
+
       const command = new InvokeCommand({
         FunctionName: "sgpu-serverless-dev-generateExcelQueue",
-        InvocationType: "Event",
-        Payload: Buffer.from(
-          JSON.stringify({
-            Records: [
-              {
-                body: JSON.stringify({
-                  type: "excel-export",
-                  dataSend: {
-                    data: resultSQL.map((object: any) => ({
-                      Código: object.pre_codigo || "",
-                      Usuario: object.usu_nomapellidos,
-                      Nombre: object.pre_nombre,
-                      "Razón social": object.cli_nomaperazsocial,
-                      Jornal: object.pre_jornal,
-                      Fecha: object.pre_fechorregistro,
-                    })),
-                    userId: userId,
-                    prefixNameFile: prefixNameFile,
-                    email: email,
-                  },
-                }),
-              },
-            ],
-          })
-        ),
+        InvocationType: "Event", // Asíncrono
+        Payload: Buffer.from(JSON.stringify(payload)),
       });
-      const response = await lambdaClient.send(command);
+
+      await lambdaClient.send(command);
     } catch (error) {
       console.error("Error al invocar la Lambda queueS3:", error);
       throw error;
